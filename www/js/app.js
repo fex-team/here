1a1962665bd2b80753da261214e3cbd8f65d2e52
zhangzimeng01@baidu.com <zhangzimeng01>
2014-03-27 12:01:06 +0800

    update

--- www/js/app.js
+++ www/js/app.js
@@ -1,52 +1,46 @@
+angular.module('hereApp', ['ionic', 'hereApp.controllers']).config(function($stateProvider, $urlRouterProvider) {
 
-angular.module('hereApp', ['ionic', 'hereApp.services', 'hereApp.controllers'])
+	$stateProvider.state('sidemenu', {
+		url : "/sidemenu",
+		abstract : true,
+		views : {
+			'pager' : {
+				templateUrl : "templates/side_menu.html"
+			}
+		}
 
+	}).state('sidemenu.home', {
+		url : "/home",
+		views : {
+			'menuContent' : {
+				templateUrl : "templates/home.html"
+			}
+		}
+	}).state('sidemenu.setting', {
+		url : "/setting",
+		views : {
+			'menuContent' : {
+				templateUrl : "templates/setting.html"
+			}
+		}
+	}).state('sidemenu.zone', {
+		url : "/zone",
+		views : {
+			'menuContent' : {
+				templateUrl : "templates/zone.html"
+			}
+		}
+	}).state('detail', {
+		url : "/detail",
+		views : {
+			'pager' : {
+				templateUrl : "templates/detail.html"
+			}
+		}
 
-.config(function($stateProvider, $urlRouterProvider) {
+	});
 
-  
-  $stateProvider
-    .state('sidemenu', {
-      url: "/sidemenu",
-      abstract: true,
-      views:{
-      	'pager':{
-      		templateUrl: "templates/side_menu.html"
-      	}
-      }
-      
-    }).state('sidemenu.home', {
-      url: "/home",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/home.html"
-        }
-      }
-    }).state('sidemenu.setting', {
-      url: "/setting",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/setting.html"
-        }
-      }
-    }).state('sidemenu.zone', {
-      url: "/zone",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/zone.html"
-        }
-      }
-    }).state('detail', {
-      url: "/detail",
-      views:{
-      	'pager':{
-      		templateUrl: "templates/detail.html"
-      	}
-      }
-      
-    });
-  
 	$urlRouterProvider.otherwise("/sidemenu/home");
-	
+
 });
 
