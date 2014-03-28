f1a6373179683e53351916a2c61d738b29c8369c
zhaochengyang <zhaochengyang@baidu.com>
2014-03-28 16:25:08 +0800

    update

--- www/js/home.js
+++ www/js/home.js
@@ -1,8 +1,17 @@
 angular.module('home', ['ionic', 'hereApp.controllers'])
 .controller('HomeController', function($scope, $ionicSideMenuDelegate) {
 
-}).controller('RecommendCollection', function($rootScope, $scope, $ionicSlideBoxDelegate, $element){
-    
+}).controller('RecommendCollection', function($rootScope, $scope, $ionicSlideBoxDelegate, $element, $timeout){
+    $scope.recommends = [{
+                            src: 'http://localhost/end/here/here/api/img?hash=/jianling/71ac93f55524725308d293d37925069d.jpg',
+                            position: '百度大厦'
+                        },{
+                            src: 'http://localhost/end/here/here/api/img?hash=/jianling/71ac93f55524725308d293d37925069d.jpg',
+                            position: '奎科大厦'
+                        },{
+                            src: 'http://localhost/end/here/here/api/img?hash=/jianling/71ac93f55524725308d293d37925069d.jpg',
+                            position: '文思海辉'
+                        }];
     $element.bind('touchstart mousedown', function(e){
         // slidemenu打开时
         if( /275/.test(angular.element(document.querySelector('#ion-pane')).css('-webkit-transform')) ){
@@ -15,6 +24,12 @@ angular.module('home', ['ionic', 'hereApp.controllers'])
     $element.bind('touchend mouseup', function(e){
         $rootScope.$broadcast('candrag', true);
     });
+
+    angular.element(document.querySelector('#recommendPosition')).html( $scope.recommends[0].position );
+
+    $scope.slide = function(){
+        angular.element(document.querySelector('#recommendPosition')).html( $scope.recommends[$scope.slideBoxController.currentIndex()].position );
+    }
 
 }).controller('SliderCollection', function($rootScope, $scope, $ionicSlideBoxDelegate, $element, $ionicSideMenuDelegate){
 
@@ -47,4 +62,18 @@ angular.module('home', ['ionic', 'hereApp.controllers'])
         angular.element(tabs).removeClass('active');
         angular.element(currentTab).addClass('active');
     });
+}).controller('HotGroup', function($scope){
+    $scope.groups = [{
+                        photos: ['./img/1.png', './img/2.png', './img/3.png', './img/4.png'],
+                        position: '北京'
+                    },{
+                        photos: ['./img/1.png', './img/2.png', './img/3.png', './img/4.png'],
+                        position: '北京'
+                    },{
+                        photos: ['./img/1.png', './img/2.png', './img/3.png', './img/4.png'],
+                        position: '北京'
+                    },{
+                        photos: ['./img/1.png', './img/2.png', './img/3.png', './img/4.png'],
+                        position: '北京'
+                    }];
 })
