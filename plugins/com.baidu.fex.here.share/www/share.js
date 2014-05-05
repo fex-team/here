8fe810f6aed6b8b9d935f1f2855d6b4631be4604
zhangzimeng01@baidu.com <zhangzimeng01>
2014-05-05 13:04:28 +0800

    update

--- plugins/com.baidu.fex.here.share/www/share.js
+++ plugins/com.baidu.fex.here.share/www/share.js
@@ -1,7 +1,7 @@
-
-
-	function getBase64FromImageUrl(URL) {
+function getBase64FromImageUrl(URL,callback) {
+		
 		var img = new Image();
+		img.crossOrigin = 'anonymous';
 		img.src = URL;
 		img.onload = function() {
 
@@ -13,36 +13,40 @@
 			ctx.drawImage(this, 0, 0);
 
 			var dataURL = canvas.toDataURL("image/png");
-
 			dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+			callback && callback(dataURL);
 
 		}
+		
 	}
-
-
-	module.exports = {
-		wechat : function(data, scene) {
-			debugger;
-			var type;
-			if (/^http:\/\//.test(data)) {
-				type = "datauri";
-				data = getBase64FromImageUrl(data);
-			}else{
-				type = "file";
-			}
-
-			cordova.exec(function() {
+	
+	function launch(type,data,scene){
+		cordova.exec(function() {
 			}, function() {
 			}, "com.baidu.fex.here.share", "wechat", [{
 				"type" : type,
 				"data" : data,
 				"scene" : scene
 			}]);
+	}
+
+	module.exports = {
+		wechat : function(data, scene) {
+			var type;
+			if (/^http:\/\//.test(data)) {
+				type = "datauri";
+				getBase64FromImageUrl(data,function(base64){
+					launch(type,base64,scene);
+				});
+			}else{
+				type = "file";
+				launch(type,data,scene);
+			}
+
+			
 		},
 		WECHAT_SCENE : {
 			session : 0,
 			timeline : 1
 		}
 	}
-
-
